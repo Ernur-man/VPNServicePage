90c7109ae70a8120d478827dc6043b511c04dc3a
Ernur-man <ernuraman926@gmail.com>
2026-08-05 09:57:02 +0500

    fix: remove unused link, fix form and main paths, update styles

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 const uls = document.querySelectorAll('.tarif ul'),
     nav = document.querySelectorAll('.plans nav'),
     select_plan = document.querySelectorAll('.tarif button'),
-    Alert = document.querySelector('Alert'),
     modal_window = document.querySelector('.modal_window'),
     get_btn = document.querySelector('.get'),
     signIn = document.querySelector(".signIn"),
@@ -26,13 +25,13 @@ signUp.addEventListener("click", ()=>{
 
 
 
-// CHECK TARIF 
+// CHECK TARIF
 const check = (a, el) =>{
     if (el.plan == uls[a].getAttribute('class')){
         const li = document.createElement('li')
         li.textContent = el.txt
         uls[a].appendChild(li)
-    }   
+    }
 }
 
 fetch('data.json')
@@ -69,21 +68,51 @@ nav.forEach((el)=>{
             x.style.borderColor = ' rgba(128, 128, 128, 0.54)'
         })
     })
-    
+
 })
 
 
 // OPEN THE MODAL WINDOW
-modal_window.addEventListener('click', (el)=>{
-    if(el.target.tagName != 'NAV' && el.target.tagName != 'INPUT'){
-        modal_window.style.display = 'none'
-    }
+
+document.querySelectorAll('.window form').forEach((form)=>{
+    form.addEventListener('submit',(event)=>{
+        event.preventDefault();
+
+        const name = form.querySelector('input[type="text"]').value.trim();
+        const phone = form.querySelectorAll('input[type="text"]')[1].value.trim();
+
+        const nameRegex = /^[A-Za-zА-Яа-яЁё\s-]{2,30}$/;
+        const phoneRegex = /^(\+7|8)\d{10}$/;
+
+        if(!nameRegex.test(name)){
+            alert("Enter correct name!")
+            return;
+        }
+        if(!phoneRegex.test(phone)){
+            alert("Enter correct phone!")
+            return;
+        }
+        alert("success!")
+
+
+        form.reset();
+        setTimeout(()=>{
+            modal_window.style.display = "none"
+        },1000)
+    })
 })
+
+
+modal_window.addEventListener('click', (e) => {
+    if (e.target === modal_window) {
+        modal_window.style.display = 'none';
+    }
+});
 
 modal_signUp.addEventListener("click", (el)=>{
     if(el.target.tagName != 'NAV' && el.target.tagName != 'INPUT'){
         modal_signUp.style.display = 'none'
-    }   
+    }
 })
 
 
